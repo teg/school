b231ab4257e519484d68cfcacfeb4dc56e13709f
Tor Erik Linnerud <tel@jklm.no>
2010-04-12 23:12:59 +0100

    Installed Compass, rewrote application.html to use HAML, some basic SASS styles

--- config/environment.rb
+++ config/environment.rb
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
+  config.gem "compass"
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
@@ -36,7 +37,6 @@ Rails::Initializer.run do |config|
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
 
-  # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-  # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-  # config.i18n.default_locale = :de
 end
+
+Haml::Template.options[:format] = :html5
